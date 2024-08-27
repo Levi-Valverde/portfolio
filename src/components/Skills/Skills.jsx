c3e59c361544cbe77b37cf1cc0b1013d66228e43
Levi-Valverde <117869426+Levi-Valverde@users.noreply.github.com>
2024-08-27 11:35:49 -0300

    Update Skills.jsx

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,8 +4,8 @@ import "./Skills.css";
 export function Skills() {
   return (
     <section className="skills section" id="skills">
-      <h2 className="section__title">Skills</h2>
-      <span className="section__subtitle">My technical level</span>
+      <h2 className="section__title">Habilidades</h2>
+      <span className="section__subtitle">Meu nível técnico</span>
 
       <div className="skills__container container grid">
         <Frontend />
