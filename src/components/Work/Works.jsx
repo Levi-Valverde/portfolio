6d45826e753287ecb90cf76e30fcb56bca455958
Levi-Valverde <117869426+Levi-Valverde@users.noreply.github.com>
2024-08-27 15:17:43 -0300

    Update Works.jsx

--- src/components/Work/Works.jsx
+++ src/components/Work/Works.jsx
@@ -4,12 +4,12 @@ import { projectNav } from "./Data";
 import { WorkItems } from "./WorkItems";
 
 export function Works() {
-  const [item, setItem] = useState({ name: "all" });
+  const [item, setItem] = useState({ name: "tudo" });
   const [projects, setProjects] = useState([]);
   const [active, setActive] = useState(0);
 
   useEffect(() => {
-    if (item.name === "all") {
+    if (item.name === "tudo") {
       setProjects(projectsData);
     } else {
       const newProjects = projectsData.filter((project) => {
