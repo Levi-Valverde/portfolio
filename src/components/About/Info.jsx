32b27fd8659c7a49dd9e38db122a16fbf4f132b1
Levi-Valverde <117869426+Levi-Valverde@users.noreply.github.com>
2024-08-27 10:29:07 -0300

    Update Info.jsx

--- src/components/About/Info.jsx
+++ src/components/About/Info.jsx
@@ -5,21 +5,21 @@ export function Info() {
     <div className="about__info grid">
       <div className="about__box">
         <i className="bx bx-award about__icon"></i>
-        <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">1 Year Working</span>
+        <h3 className="about__title">Experiência</h3>
+        <span className="about__subtitle">1 ano</span>
       </div>
 
       <div className="about__box">
         <i className="bx bx-briefcase-alt about__icon"></i>
 
-        <h3 className="about__title">Completed</h3>
-        <span className="about__subtitle">20 + Projects</span>
+        <h3 className="about__title">Completado</h3>
+        <span className="about__subtitle">20 + Projetos</span>
       </div>
 
       <div className="about__box">
         <i className="bx bx-support about__icon"></i>
 
-        <h3 className="about__title">Suport</h3>
+        <h3 className="about__title">Suporte</h3>
         <span className="about__subtitle">Online 24/7</span>
       </div>
     </div>
