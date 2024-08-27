9dddbf6fd5a034c305b6647e5e461c10466369d8
Levi-Valverde <117869426+Levi-Valverde@users.noreply.github.com>
2024-08-27 10:39:28 -0300

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -19,12 +19,12 @@ export function Contact() {
 
   return (
     <section className="contact section" id="contact">
-      <h2 className="section__title">Get in touch</h2>
-      <span className="section__subtitle">Contact Me</span>
+      <h2 className="section__title">Entre em Contato</h2>
+      <span className="section__subtitle">Comunicação</span>
 
       <div className="contact__container container grid">
         <div className="contact__content">
-          <h3 className="contact__title">Talk to me</h3>
+          <h3 className="contact__title">Fale comigo</h3>
 
           <div className="contact__info">
             <div className="contact__card">
