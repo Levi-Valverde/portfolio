1bc5b757a532d1089fc6c4283d3daa87b411c1fa
Levi-Valverde <117869426+Levi-Valverde@users.noreply.github.com>
2025-10-06 00:19:59 -0300

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./About.css";
 import AboutImg from "../../assets/levi-valverde-lima-2.jpeg";
-import CV from "../../assets/Curriculum4.pdf";
+import CV from "../../assets/Curriculum3.pdf";
 import { Info } from "./Info";
 
 export function About() {
@@ -53,5 +53,6 @@ export function About() {
     </section>
   );
 }
+
 
 
